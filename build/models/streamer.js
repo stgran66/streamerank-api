bad408d2f0b47540e8dd245db67c47dd4cd8adf1
stgran66 <stanislaw.grygoriew@gmail.com>
2023-06-28 15:17:17 +0200

    allowed empty string for description

--- build/models/streamer.js
+++ build/models/streamer.js
@@ -1,48 +1,51 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 const { Schema, model } = require('mongoose');
 const Joi = require('joi');
 // Valid streamer's platforms
 const platforms = ['twitch', 'youtube', 'tiktok', 'kick', 'rumble'];
-const streamerSchema = new Schema({
+const streamerSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Set streamer's name"],
+      type: String,
+      required: [true, "Set streamer's name"],
     },
     platform: {
-        type: String,
-        enum: platforms,
-        required: [true, "Set streamer's platform"],
+      type: String,
+      enum: platforms,
+      required: [true, "Set streamer's platform"],
     },
     description: { type: String },
     upvote: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     downvote: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     avatar: {
-        type: String,
-        default: 'https://i.pravatar.cc/300',
+      type: String,
+      default: 'https://i.pravatar.cc/300',
     },
-}, { versionKey: false });
+  },
+  { versionKey: false }
+);
 // Joi schema for adding streamer
 const addStreamerSchema = Joi.object({
-    name: Joi.string().required(),
-    platform: Joi.string()
-        .required()
-        .valid(...platforms),
-    description: Joi.string(),
+  name: Joi.string().required(),
+  platform: Joi.string()
+    .required()
+    .valid(...platforms),
+  description: Joi.string().allow('', null),
 });
 // Joi schema for voting for streamer
 const voteSchema = Joi.object({
-    vote: Joi.string().valid('upvote', 'downvote').required(),
+  vote: Joi.string().valid('upvote', 'downvote').required(),
 });
 const streamerSchemas = {
-    addStreamerSchema,
-    voteSchema,
+  addStreamerSchema,
+  voteSchema,
 };
 const Streamer = model('streamer', streamerSchema);
 module.exports = { Streamer, streamerSchemas };
